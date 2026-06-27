bcc1e036d7d31d0239c9c143633801806c834389
Hryhorii Markevych <gregorymark.kjk@gmail.com>
2026-06-27 14:53:09 +0200

    Initial commit with Playwright POM and Allure

---
+++ tests/specs/login.spec.ts
@@ -0,0 +1,26 @@
+import { LoginPage } from "../../pages/login.page";
+import { test, expect } from "@playwright/test"; 
+
+test.describe("Login Page", () => {
+  let loginPage: LoginPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.navigateToLoginPage();
+  });
+
+  test("Login with valid credentials", async ({ page }) => {
+    await loginPage.login("tester69", "Tester69"); 
+    await expect(page).toHaveURL("https://www.redmine.org/my/twofa/totp/activate/confirm");
+  });
+
+  test("Login with empty credentials", async ({ page }) => {
+    await loginPage.login("", ""); 
+    await expect(page.locator("#flash_error")).toBeVisible();
+  });
+
+  test("Login with invalid credentials", async ({ page }) => {
+    await loginPage.login("123", "Tester69"); 
+    await expect(page.locator("#flash_error")).toBeVisible();
+  });
+});
