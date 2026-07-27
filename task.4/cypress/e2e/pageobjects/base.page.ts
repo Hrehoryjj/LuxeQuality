b2c21f7e7ecabdbed0d70ff5edbbb3f80991bb62
Hryhorii Markevych <gregorymark.kjk@gmail.com>
2026-07-27 20:27:37 +0200

    try to fix test1

--- task.4/cypress/e2e/pageobjects/base.page.ts
+++ task.4/cypress/e2e/pageobjects/base.page.ts
@@ -3,5 +3,28 @@ export class BasePage {
 
     navigateTo(url: string): void {
         cy.visit(url);
+        this.dismissCookieBannerIfPresent();
+    }
+
+    private dismissCookieBannerIfPresent(): void {
+        cy.document().then((doc) => {
+            return new Cypress.Promise((resolve) => {
+                const checkInterval = setInterval(() => {
+                    const acceptButton = doc.querySelector(BasePage.COOKIE_BANNER_ACCEPT_BUTTON);
+                    if (acceptButton) {
+                        clearInterval(checkInterval);
+                        resolve(true);
+                    }
+                }, 100);
+                setTimeout(() => {
+                    clearInterval(checkInterval);
+                    resolve(false);
+                }, 3000);
+            });
+        }).then((bannerFound) => {
+            if (bannerFound) {
+                cy.get(BasePage.COOKIE_BANNER_ACCEPT_BUTTON).click();
+            }
+        });
     }
 }
