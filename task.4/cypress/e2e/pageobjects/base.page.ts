6e17b78e8d6712358e29f1503e4c8824ea9e2a28
Hryhorii Markevych <gregorymark.kjk@gmail.com>
2026-07-27 13:34:25 +0200

    Wait for cookie banner to render before dismissing (avoid fixed delay)

--- task.4/cypress/e2e/pageobjects/base.page.ts
+++ task.4/cypress/e2e/pageobjects/base.page.ts
@@ -1,17 +1,28 @@
 export class BasePage {
     private static readonly COOKIE_BANNER_CLOSE_BUTTON = '#onetrust-close-btn-container button.onetrust-close-btn-handler';
-
     navigateTo(url: string): void {
         cy.visit(url);
         this.dismissCookieBannerIfPresent();
     }
-
-    private dismissCookieBannerIfPresent(): void {
-        cy.get('body').then(($body) => {
-            const closeButton = $body.find(BasePage.COOKIE_BANNER_CLOSE_BUTTON);
-            if (closeButton.length > 0) {
-                cy.wrap(closeButton.first()).click();
-            }
+   private dismissCookieBannerIfPresent(): void {
+    cy.document().then((doc) => {
+        return new Cypress.Promise((resolve) => {
+            const checkInterval = setInterval(() => {
+                const closeButton = doc.querySelector(BasePage.COOKIE_BANNER_CLOSE_BUTTON);
+                if (closeButton) {
+                    clearInterval(checkInterval);
+                    resolve(true);
+                }
+            }, 100);
+            setTimeout(() => {
+                clearInterval(checkInterval);
+                resolve(false);
+            }, 3000);
         });
-    }
+    }).then((bannerFound) => {
+        if (bannerFound) {
+            cy.get(BasePage.COOKIE_BANNER_CLOSE_BUTTON).click();
+        }
+    });
+}
 }
