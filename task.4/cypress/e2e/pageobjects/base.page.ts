a9393886c0d1c6b07811cb3415700620af857ad3
Hryhorii Markevych <gregorymark.kjk@gmail.com>
2026-07-27 13:40:26 +0200

    Wait for cookie banner to render before dismissing (avoid fixed delay)with fixes

--- task.4/cypress/e2e/pageobjects/base.page.ts
+++ task.4/cypress/e2e/pageobjects/base.page.ts
@@ -1,28 +1,30 @@
 export class BasePage {
-    private static readonly COOKIE_BANNER_CLOSE_BUTTON = '#onetrust-close-btn-container button.onetrust-close-btn-handler';
+    private static readonly COOKIE_BANNER_ACCEPT_BUTTON = '#onetrust-accept-btn-handler';
+
     navigateTo(url: string): void {
         cy.visit(url);
         this.dismissCookieBannerIfPresent();
     }
-   private dismissCookieBannerIfPresent(): void {
-    cy.document().then((doc) => {
-        return new Cypress.Promise((resolve) => {
-            const checkInterval = setInterval(() => {
-                const closeButton = doc.querySelector(BasePage.COOKIE_BANNER_CLOSE_BUTTON);
-                if (closeButton) {
+
+    private dismissCookieBannerIfPresent(): void {
+        cy.document().then((doc) => {
+            return new Cypress.Promise((resolve) => {
+                const checkInterval = setInterval(() => {
+                    const acceptButton = doc.querySelector(BasePage.COOKIE_BANNER_ACCEPT_BUTTON);
+                    if (acceptButton) {
+                        clearInterval(checkInterval);
+                        resolve(true);
+                    }
+                }, 100);
+                setTimeout(() => {
                     clearInterval(checkInterval);
-                    resolve(true);
-                }
-            }, 100);
-            setTimeout(() => {
-                clearInterval(checkInterval);
-                resolve(false);
-            }, 3000);
+                    resolve(false);
+                }, 3000);
+            });
+        }).then((bannerFound) => {
+            if (bannerFound) {
+                cy.get(BasePage.COOKIE_BANNER_ACCEPT_BUTTON).click();
+            }
         });
-    }).then((bannerFound) => {
-        if (bannerFound) {
-            cy.get(BasePage.COOKIE_BANNER_CLOSE_BUTTON).click();
-        }
-    });
-}
+    }
 }
